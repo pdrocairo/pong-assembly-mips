1541ee869e601c6e9da5f221045d583186a5130b
pdrocairo <pedro.cairo21@gmail.com>
2026-06-14 19:34:07 -0300

    Finalizado animacao lider de torcida

--- PONG.asm
+++ PONG.asm
@@ -106,12 +106,127 @@ dados_lateral2:
 	li $15, 44     
 	li $8, 0x100113D4
 for_lateral2:
-	beq $5, $15, fim
+	beq $5, $15, dados_stick
 	sw $6, 0($8)
 	
 	addi $8, $8, 512
 	addi $5, $5, 1
 	j for_lateral2	
-			
+
+# Endereco = base + (linha* 128 + coluna) * 4
+dados_stick:
+	li $6, branco
+	li $14, 0x10017248
+	li $15, 10            
+	li $16, 0            
+
+for_sticks:
+	beq $16, $15, dados_pes
+
+	li $5, 0             
+	move $8, $14        
+
+for_linhas:
+	beq $5, 5, proxima_coluna   
+	sw $6, 0($8)
+	addi $8, $8, 512    
+	addi $5, $5, 1
+	j for_linhas
+
+proxima_coluna:
+	addi $14, $14, 40   
+	addi $16, $16, 1
+	j for_sticks
+	
+dados_pes:
+	li $14, 0x10017C44 #pe inicial esquerdo
+	li $17, 0x10017C4C #pe inicial direito	
+	li $6, branco
+	li $15, 10            
+	li $16, 0
+
+for_pes:
+	beq $16, $15, loop_animacao
+	sw $6, 0($14)
+	sw $6, 0($17)
+	
+	addi $16, $16, 1
+	addi $14, $14, 40
+	addi $17, $17, 40
+	j for_pes
+	
+loop_animacao:
+	li $6, 0x00FF8DA1
+	li $7, preto
+	li $15, 10            
+	li $16, 0
+	li $14, 0x10017844
+	li $17, 0x1001764C
+	
+desenha_braco_esq:
+	beq $16, $15, delay1
+	sw $6, 0($14)
+	sw $6, 0($17)
+	addi $16, $16, 1
+	addi $14, $14, 40
+	addi $17, $17, 40
+	j desenha_braco_esq
+
+delay1:
+	li $25, 300000
+delay1_loop:
+	beq $25, $0, apaga_ini1
+	addi $25, $25, -1
+	j delay1_loop
+
+apaga_ini1:
+	li $15, 10            
+	li $16, 0
+	li $14, 0x10017844
+	li $17, 0x1001764C
+apaga_braco_esq:
+	beq $16, $15, dados_bracos_dir
+	sw $7, 0($14)
+	sw $7, 0($17)
+	addi $16, $16, 1
+	addi $14, $14, 40
+	addi $17, $17, 40
+	j apaga_braco_esq
+
+dados_bracos_dir:
+	li $15, 10            
+	li $16, 0
+	li $14, 0x10017644
+	li $17, 0x1001784C
+desenha_braco_dir:
+	beq $16, $15, delay2
+	sw $6, 0($14)
+	sw $6, 0($17)
+	addi $16, $16, 1
+	addi $14, $14, 40
+	addi $17, $17, 40
+	j desenha_braco_dir
+
+delay2:
+	li $25, 300000
+delay2_loop:
+	beq $25, $0, apaga_ini2
+	addi $25, $25, -1
+	j delay2_loop
+
+apaga_ini2:
+	li $15, 10            
+	li $16, 0
+	li $14, 0x10017644
+	li $17, 0x1001784C
+apaga_braco_dir:
+	beq $16, $15, loop_animacao
+	sw $7, 0($14)
+	sw $7, 0($17)
+	addi $16, $16, 1
+	addi $14, $14, 40
+	addi $17, $17, 40
+	j apaga_braco_dir
+
 fim:
 	j fim
